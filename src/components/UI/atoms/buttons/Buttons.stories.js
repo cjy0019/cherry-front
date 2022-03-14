bc9b33a5afcab6aeb143d79c39cafd7b4a84c953
JYCHO <cjy0029@naver.com>
2022-03-14 18:42:30 +0900

    feat : 만족도 버튼 구현

--- src/components/UI/atoms/buttons/Buttons.stories.js
+++ src/components/UI/atoms/buttons/Buttons.stories.js
@@ -1,11 +1,11 @@
 import CherryPickStartButton from './CherryPickStartButton';
 import RetryButton from './RetryButton';
 import ReviewButton from './ReviewButton';
+import SatisficationButton from './SatisficationButton';
 import SaveButton from './SaveButton';
 
 export default {
   title: 'Buttons',
-  component: ReviewButton,
 };
 
 export const Retry = () => <RetryButton />;
@@ -14,5 +14,17 @@ export const WriteReview = () => <ReviewButton>리뷰 작성하기</ReviewButton
 export const CompleteReview = () => (
   <ReviewButton dim>리뷰 작성완료</ReviewButton>
 );
-export const Save = () => <SaveButton>저장하기</SaveButton>;
-export const SaveDim = () => <SaveButton dim>저장하기</SaveButton>;
+export const Save = () => (
+  <div style={{ display: 'flex', gap: '10px', flexDirection: 'column' }}>
+    <SaveButton>저장하기</SaveButton>
+    <SaveButton dim>저장하기</SaveButton>
+  </div>
+);
+export const SatisficationVeryGood = () => (
+  <div style={{ display: 'flex', gap: '10px' }}>
+    <SatisficationButton point>매우 만족</SatisficationButton>
+    <SatisficationButton>만족</SatisficationButton>
+    <SatisficationButton>보통</SatisficationButton>
+    <SatisficationButton>그저 그럼</SatisficationButton>
+  </div>
+);
---
+++ src/components/UI/atoms/buttons/SatisficationButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import palette from '../../../../style/palette';
+
+const SatisficationButton = ({ children, point }) => {
+  return <StyledButton point={point}>{children}</StyledButton>;
+};
+
+const StyledButton = styled.button`
+  background-color: #000000;
+  padding: 10px;
+  border-radius: 3px;
+  font-weight: 400;
+  color: ${palette.textWhite};
+  border: none;
+
+  ${(props) =>
+    props.point &&
+    css`
+      color: ${palette.pointRed};
+    `}
+`;
+
+export default SatisficationButton;
