0e0df9f19439a536b57c31c406ae59ec24b94ae3
JYCHO <cjy0029@naver.com>
2022-03-14 17:33:37 +0900

    feat : 저장하기 버튼 UI 및 스토리북 작성완료

--- src/components/UI/atoms/buttons/Buttons.stories.js
+++ src/components/UI/atoms/buttons/Buttons.stories.js
@@ -1,6 +1,7 @@
 import CherryPickStartButton from './CherryPickStartButton';
 import RetryButton from './RetryButton';
 import ReviewButton from './ReviewButton';
+import SaveButton from './SaveButton';
 
 export default {
   title: 'Buttons',
@@ -11,5 +12,7 @@ export const Retry = () => <RetryButton />;
 export const CherryPickStart = () => <CherryPickStartButton />;
 export const WriteReview = () => <ReviewButton>리뷰 작성하기</ReviewButton>;
 export const CompleteReview = () => (
-  <ReviewButton point>리뷰 작성완료</ReviewButton>
+  <ReviewButton dim>리뷰 작성완료</ReviewButton>
 );
+export const Save = () => <SaveButton>저장하기</SaveButton>;
+export const SaveDim = () => <SaveButton dim>저장하기</SaveButton>;
--- src/components/UI/atoms/buttons/SaveButton.jsx
+++ src/components/UI/atoms/buttons/SaveButton.jsx
@@ -3,19 +3,20 @@ import styled, { css } from 'styled-components';
 import palette from '../../../../style/palette';
 import { responsive } from '../../../../style/responsive';
 
-const ReviewButton = ({ children, dim }) => {
+const SaveButton = ({ children, dim }) => {
   return <StyledButton dim={dim}>{children}</StyledButton>;
 };
 
 const StyledButton = styled.button`
-  width: 308px;
-  height: 40px;
-  border: none;
+  width: 768px;
+  height: 44px;
   border-radius: 4px;
-  color: ${palette.textWhite};
   background-color: ${palette.pointRed};
   font-weight: 700;
+  color: ${palette.textWhite};
   font-size: 12px;
+  border: none;
+  cursor: pointer;
 
   ${(props) =>
     props.dim &&
@@ -24,9 +25,13 @@ const StyledButton = styled.button`
       color: ${palette.text6};
     `}
 
+  @media ${responsive.tablet} {
+    width: 718px;
+  }
+
   @media ${responsive.mobile} {
-    width: 152px;
+    width: 340px;
   }
 `;
 
-export default ReviewButton;
+export default SaveButton;
