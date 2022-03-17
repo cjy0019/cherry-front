f1ec6fe51ac77aa4b070ecd3de5cb9a83b2989c9
EOJIN KIM <kimeojin1995@gmail.com>
2022-03-17 16:53:11 +0900

    feature : 아토믹 디자인 강의검색 Input "#12"

---
+++ src/components/UI/atoms/input/SearchInput.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import palette from '../../../../style/palette';
+import searchRed from '../../../../assets/img/search_red.svg';
+import closeRed from '../../../../assets/img/close_red.svg';
+import { responsive } from '../../../../style/responsive';
+
+const SearchInput = ({ handleSubmit, searched }) => {
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Input placeholder='찾의시는 강의, 강사가 있으신가요?' />
+      <SubmitButton
+        searched={searched}
+        value
+        type='submit'
+        alt='submit'
+        value=''
+      />
+    </Form>
+  );
+};
+
+const Form = styled.form`
+  display: inline-block;
+  position: relative;
+`;
+
+const Input = styled.input`
+  background-color: ${palette.subNavy2};
+
+  padding: 16px 156px 16px 20px;
+  border-radius: 100px;
+  border: none;
+
+  color: ${palette.textWhite};
+  font-weight: 700;
+  font-size: 14px;
+
+  &::placeholder {
+    font-weight: 400;
+    font-size: 12px;
+    color: ${palette.textWhite};
+  }
+
+  @media ${responsive.mobile} {
+    padding: 16px 140px 16px 16px;
+  }
+
+  & > button {
+    border: none;
+  }
+`;
+
+const SubmitButton = styled.input`
+  width: 28px;
+  height: 28px;
+
+  background: url(${searchRed});
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  ${({ searched }) =>
+    searched &&
+    css`
+      background: url(${closeRed});
+    `}
+
+  svg {
+    fill: ${palette.pointRed};
+  }
+
+  border: none;
+
+  position: absolute;
+  top: 10px;
+  right: 9px;
+`;
+
+export default SearchInput;
