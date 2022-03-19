9e8af4d7bdb499041a8c12d862e94b2c8c6eb853
JYCHO <cjy0029@naver.com>
2022-03-19 16:03:17 +0900

    feat : 푸터 아토믹 UI 1차 구현 완료 & 스토리북 콘솔 애드온 추가

--- src/components/molecules/footer/Footer.jsx
+++ src/components/molecules/footer/Footer.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import palette from '../../../style/palette';
+import { responsive } from '../../../style/responsive';
 
 const Footer = () => {
+  const [viewPort, setViewPort] = useState(window.innerWidth);
+
+  const resizeViewPort = useCallback(
+    (e) => {
+      setViewPort(window.innerWidth);
+      e.stopPropagation();
+    },
+    [viewPort],
+  );
+
+  useEffect(() => {
+    window.addEventListener('resize', resizeViewPort);
+
+    return () => window.removeEventListener('resize', resizeViewPort);
+  }, [viewPort]);
+
   return (
     <StyledFooter>
       <ContentsContainer>
@@ -11,9 +28,11 @@ const Footer = () => {
           <BetaBadge>BETA</BetaBadge>
           <StyledSmall>&#9426; 2022 Cherry Pick</StyledSmall>
         </FlexWrapper>
+
         <FlexWrapper>
           <StyledP>email</StyledP>
           <EmailBadge>CherryPick@gmail.com</EmailBadge>
+          {viewPort <= 400 && <Line />}
           <StyledP>github</StyledP>
           <EmailBadge>github.com/cherrypick-project</EmailBadge>
         </FlexWrapper>
@@ -23,18 +42,31 @@ const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: center;
   width: 100%;
   background-color: ${palette.footerBlack};
   color: white;
+  padding: 12px 0;
 `;
 
 const ContentsContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  gap: 30px;
+  width: 1100px;
+  box-sizing: border-box;
+  padding-left: 13px;
 
-  & > div:nth-of-type(2) {
-    gap: 8px;
+  @media ${responsive.tablet} {
+    width: 100%;
+    padding-right: 3px;
+  }
+
+  @media ${responsive.mobile} {
+    flex-direction: column-reverse;
+    gap: 0;
   }
 `;
 
@@ -47,6 +79,11 @@ const CherryPickText = styled.p`
   color: ${palette.pointRed};
   font-size: 16px;
   font-weight: 400;
+  white-space: nowrap;
+
+  @media ${responsive.mobile} {
+    font-size: 12px;
+  }
 `;
 
 const BetaBadge = styled.div`
@@ -62,19 +99,38 @@ const BetaBadge = styled.div`
   text-align: center;
   margin-left: 8px;
   color: ${palette.textWhite};
+  padding-top: 2px;
+
+  @media ${responsive.mobile} {
+    width: 40px;
+    height: 18px;
+    margin-left: 4px;
+  }
 `;
 
 const StyledSmall = styled.small`
   font-weight: 400;
   font-size: 12px;
   color: ${palette.text5};
+  white-space: nowrap;
   margin-left: 12px;
+
+  @media ${responsive.mobile} {
+    font-size: 10px;
+    margin: 0;
+  }
 `;
 
 const StyledP = styled.p`
   font-weight: 400;
   font-size: 12px;
   color: ${palette.text5};
+  margin-right: 8px;
+
+  @media ${responsive.mobile} {
+    margin-right: 4px;
+    font-size: 10px;
+  }
 `;
 
 const EmailBadge = styled.div`
@@ -86,6 +142,21 @@ const EmailBadge = styled.div`
   color: ${palette.textWhite};
   border: 1px solid rgba(255, 255, 255, 0.08);
   border-radius: 6px;
+  white-space: nowrap;
+
+  @media ${responsive.mobile} {
+    font-size: 10px;
+    border: none;
+    padding: 0;
+  }
 `;
 
-export default Footer;
+const Line = styled.div`
+  width: 10px;
+  height: 0px;
+  opacity: 0.2;
+  border: 1px solid #ffffff;
+  transform: rotate(90deg);
+  margin: 0 6px;
+`;
+export default React.memo(Footer);
--- src/style/responsive.js
+++ src/style/responsive.js
@@ -1,5 +1,5 @@
 export const responsive = {
-  mobile: '(max-width: 360px)',
+  mobile: '(max-width: 400px)',
   tablet: '(min-width: 361px) and (max-width: 768px)',
   laptop: '(min-width: 769px)',
 };
