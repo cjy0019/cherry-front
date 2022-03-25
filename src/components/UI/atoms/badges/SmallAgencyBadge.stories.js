baa4ed0b377c546f606e3ec65e6cca1e463597a4
JYCHO <cjy0029@naver.com>
2022-03-25 13:04:02 +0900

    refactor : 모바일 아이패드용 작은 뱃지 생성 [#24]

---
+++ src/components/UI/atoms/badges/SmallAgencyBadge.stories.js
@@ -0,0 +1,17 @@
+import SmallAgencyBadge from './SmallAgencyBadge';
+
+export default {
+  title: 'atoms/뱃지/기관 & 강사이름 뱃지',
+  component: SmallAgencyBadge,
+};
+
+const Template = (args) => <SmallAgencyBadge {...args} />;
+
+export const SmallAgency = Template.bind({});
+
+SmallAgency.args = {
+  children: '기관 groomedu',
+};
+
+SmallAgency.storyName =
+  '메인 ipad 카드 3개 배치, 메인 모바일 리스트 형태, 상세 아이패드';
