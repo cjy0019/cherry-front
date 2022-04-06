f99add369542331460c07defd6dabde0f7ce0e55
EOJIN KIM <kimeojin1995@gmail.com>
2022-04-06 17:25:16 +0900

    fix : StoryBook 폰트 미적용 버그 수정 / 스토리북에서만 폰트 적용되지 않는 문제 수정 "#36"

--- src/style/GlobalStyle.js
+++ src/style/GlobalStyle.js
@@ -23,57 +23,68 @@ const globalStyle = css`
     font-family: 'Roboto';
     src: url(${RobotoBold}) format('woff');
     unicode-range: U+0041-005A, U+0061-007A, U+0030-0039;
+    font-weight: 700;
   }
   @font-face {
     font-family: 'Roboto';
     src: url(${RobotoMedium}) format('woff');
     unicode-range: U+0041-005A, U+0061-007A, U+0030-0039;
+    font-weight: 500;
   }
   @font-face {
     font-family: 'Roboto';
     src: url(${RobotoRegular}) format('woff');
     unicode-range: U+0041-005A, U+0061-007A, U+0030-0039;
+    font-weight: 400;
   }
   @font-face {
     font-family: 'Roboto';
     src: url(${RobotoLight}) format('woff');
     unicode-range: U+0041-005A, U+0061-007A, U+0030-0039;
+    font-weight: 300;
   }
   @font-face {
     font-family: 'Roboto';
     src: url(${RobotoThin}) format('woff');
     unicode-range: U+0041-005A, U+0061-007A, U+0030-0039;
+    font-weight: 100;
   }
 
   @font-face {
     font-family: 'Pretendard';
     src: url(${PretendardBold}) format('woff');
     unicode-range: U+AC00-D7A3;
+    font-weight: 700;
   }
   @font-face {
     font-family: 'Pretendard';
     src: url(${PretendardSemiBold}) format('woff');
     unicode-range: U+AC00-D7A3;
+    font-weight: 600;
   }
   @font-face {
     font-family: 'Pretendard';
     src: url(${PretendardMedium}) format('woff');
     unicode-range: U+AC00-D7A3;
+    font-weight: 500;
   }
   @font-face {
     font-family: 'Pretendard';
     src: url(${PretendardRegular}) format('woff');
     unicode-range: U+AC00-D7A3;
+    font-weight: 400;
   }
   @font-face {
     font-family: 'Pretendard';
     src: url(${PretendardLight}) format('woff');
     unicode-range: U+AC00-D7A3;
+    font-weight: 300;
   }
   @font-face {
     font-family: 'Pretendard';
     src: url(${PretendardThin}) format('woff');
     unicode-range: U+AC00-D7A3;
+    font-weight: 100;
   }
 
   body {
