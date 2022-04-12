3ceae3a1ab313ac48e5cbb6e9804de463ac032cb
JYCHO <cjy0029@naver.com>
2022-04-12 22:51:59 +0900

    fix : detail 페이지 ui 반응형 리셋

--- src/components/templates/DetailTemplate.jsx
+++ src/components/templates/DetailTemplate.jsx
@@ -1,43 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import LectureInfo from '../molecules/detail/LectureInfo';
-import Header from '../molecules/header/Header';
-import LecturePrice from '../molecules/detail/LecturePrice';
-import palette from '../../style/palette';
-import SideBar from '../molecules/detail/SideBar';
-import ScoreAverage from '../molecules/detail/ScoreAverage';
-
 const DetailTemplate = () => {
   return (
     <>
       <Header NotMain />
-      <Container>
-        <LectureInfo />
-        <VerticalDivision>
-          <div>
-            <LecturePrice />
-            <ScoreAverage />
-          </div>
-          <SideBar />
-        </VerticalDivision>
-      </Container>
+      <div></div>
     </>
   );
 };
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-color: ${palette.backgroundBlack};
-`;
-
-const VerticalDivision = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 100%;
-  gap: 45px;
-`;
 
 export default DetailTemplate;
