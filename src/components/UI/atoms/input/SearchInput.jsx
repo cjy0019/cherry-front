70617376692ffc085aed1f1036df37a0a5e21d65
EOJIN KIM <kimeojin1995@gmail.com>
2022-04-12 18:00:31 +0900

    refactor : Header 반응형 디자인 리팩토링 "39"

--- src/components/UI/atoms/input/SearchInput.jsx
+++ src/components/UI/atoms/input/SearchInput.jsx
@@ -18,39 +18,56 @@ const SearchInput = ({ className, handleSubmit, searched, main }) => {
 const Form = styled.form`
   display: inline-block;
   position: relative;
+
+  margin-right: 16px;
 `;
 
 const Input = styled.input`
-  background-color: ${palette.subNavy2};
+  all: unset;
+  display: block;
   outline: none;
 
-  /* width pc 340px */
+  /* width: 270px; */
   width: 270px;
-  padding: 16px 50px 16px 20px;
+  padding: 14px 50px 14px 20px;
+
   border-radius: 100px;
   border: none;
 
+  background-color: ${palette.subNavy2};
+
   color: ${palette.textWhite};
   font-weight: 700;
-  font-size: 14px;
+  font-size: 0.875rem;
+  font-family: 'Pretendard';
 
   &::placeholder {
     font-weight: 400;
-    font-size: 12px;
+    font-size: 0.75rem;
     color: ${palette.textWhite};
     opacity: 0.6;
   }
 
+  /* PC 뷰포트 985px 영역부터 로그인, 회원가입 텍스트가 SearchInput 부분을 침범하기 시작하므로 width 줄이기*/
+  @media (min-width: 769px) and (max-width: 985px) {
+    width: 27.411vw;
+  }
+
+  /* 태블릿 뷰포트 650px 영역부터 로그인, 회원가입 텍스트가 SearchInput 부분을 침범하기 시작하므로 width 줄이기*/
+  @media ${responsive.tablet} {
+    width: 35.156vw;
+  }
+
   @media ${responsive.mobile} {
     // 메인 페이지 외 280px
-    width: 210px;
-    padding: 16px 50px 16px 20px;
+    width: 48.837vw;
+    padding: 13.5px 50px 13.5px 20px;
 
     // 메인 페이지 모바일인 경우
     ${({ main }) =>
       main &&
       css`
-        width: 254px;
+        width: 59.07vw;
         padding: 16px 50px 16px 16px; // 메인 320px
       `}
   }
@@ -62,7 +79,9 @@ const Input = styled.input`
 
 const SubmitButton = styled.input`
   position: absolute;
-  top: 10px;
+
+  /* top:8px, right:9px */
+  top: 8px;
   right: 9px;
 
   width: 28px;
