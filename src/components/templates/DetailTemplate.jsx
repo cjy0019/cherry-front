97d678d7ca47be894aa1afb3e1c57cc02aaef4fd
JYCHO <cjy0029@naver.com>
2022-04-13 16:06:09 +0900

    feat : 강의 정보 파트 UI 구현 완료 [#34]

--- src/components/templates/DetailTemplate.jsx
+++ src/components/templates/DetailTemplate.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import BackgroundOpacity from '../molecules/detail/BackgroundOpacity';
+import styled from 'styled-components';
 
 import Header from '../molecules/header/Header';
 import LectureDetailTitle from '../molecules/detail/LectureDetailTitle';
+import LectureInfo from '../molecules/detail/LectureInfo';
 
 const DetailTemplate = () => {
   return (
@@ -10,8 +12,25 @@ const DetailTemplate = () => {
       <Header NotMain />
       <BackgroundOpacity />
       <LectureDetailTitle />
+      <VerticalContainer>
+        <div>
+          <LectureInfo />
+        </div>
+        <SideBar>sidebar</SideBar>
+      </VerticalContainer>
     </>
   );
 };
+
+const VerticalContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 49px;
+`;
+
+const SideBar = styled.div`
+  background-color: yellowgreen;
+  width: 376px;
+`;
 
 export default DetailTemplate;
