92f787924922c712692d08bc4676d83d1a8df400
JYCHO <cjy0029@naver.com>
2022-04-16 22:45:27 +0900

    feat : 리뷰 작성하기 페이지 스토리 작성[#45]

---
+++ src/components/templates/stories/ReviewWriteTemplate.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReviewWriteTemplate from '../ReviewWriteTemplate';
+
+export default {
+  title: 'templates/리뷰 작성 페이지',
+  component: ReviewWriteTemplate,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const ReviewWrite = () => <ReviewWriteTemplate />;
+
+ReviewWrite.storyName = '리뷰 작성 페이지';
