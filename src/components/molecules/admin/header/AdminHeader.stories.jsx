c9410f86209d354cc366cdea3c383d8c362a6c9a
JYCHO <cjy0029@naver.com>
2022-04-22 14:58:14 +0900

    feat : 어드민 헤더 UI 스토리 작성완료[#63]

---
+++ src/components/molecules/admin/header/AdminHeader.stories.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AdminHeader from './AdminHeader';
+
+export default {
+  title: 'molecules/어드민/어드민 헤더',
+  component: AdminHeader,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  decorators: [
+    (Story) => (
+      <BrowserRouter>
+        <div style={{ marginTop: '3%' }}>
+          <Story />
+        </div>
+      </BrowserRouter>
+    ),
+  ],
+};
+
+const Template = (args) => <AdminHeader {...args} />;
+
+export const AdminHeaderStory = Template.bind({});
+
+AdminHeaderStory.storyName = '어드민 헤더';
