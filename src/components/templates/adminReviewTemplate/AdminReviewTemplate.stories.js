057e2e1c7be34033272d2bd8001de9411bd5d233
EOJIN KIM <kimeojin1995@gmail.com>
2022-05-17 18:00:25 +0900

    feature : 리뷰 관리 페이지 / UI 구현 "#85"

---
+++ src/components/templates/adminReviewTemplate/AdminReviewTemplate.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AdminReviewTemplate from './AdminReviewTemplate';
+
+export default {
+  title: 'templates/리뷰 관리',
+  component: AdminReviewTemplate,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const AdminReviewTemplateStory = () => (
+  <BrowserRouter>
+    <AdminReviewTemplate />
+  </BrowserRouter>
+);
+
+AdminReviewTemplateStory.storyName = '리뷰 관리';
