0fbdf99ed53e3a8dc508a169f536925e662fa2b2
EOJIN KIM <kimeojin1995@gmail.com>
2022-05-19 18:26:26 +0900

    feature: 관리자 통계 분석 / 반응형 UI 구현 "#85"

---
+++ src/components/templates/adminReviewDetailTemplate/AdminReviewDetailTemplate.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AdminReviewDetailTemplate from './AdminReviewDetailTemplate';
+
+export default {
+  title: 'templates/관리자 리뷰 상세',
+  component: AdminReviewDetailTemplate,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const AdminReviewDetailTemplateStory = () => (
+  <BrowserRouter>
+    <AdminReviewDetailTemplate />
+  </BrowserRouter>
+);
+
+AdminReviewDetailTemplateStory.storyName = '관리자 리뷰 상세';
---
+++ src/components/templates/adminStatisticsTemplate/AdminStatisticsTemplate.stories.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AdminStatisticsTemplate from './AdminStatisticsTemplate';
+
+export default {
+  title: 'templates/관리자 통계 분석 페이지',
+  component: AdminStatisticsTemplate,
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+export const AdminStatisticsTemplateStory = () => (
+  <BrowserRouter>
+    <AdminStatisticsTemplate />
+  </BrowserRouter>
+);
+
+AdminStatisticsTemplateStory.storyName = '관리자 통계 분석 페이지';
