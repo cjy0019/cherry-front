a333173e627cac57fc8b581877f24bf939466b30
Jayden <cjy0029@naver.com>
2022-07-01 10:14:50 +0900

    feat : admin 핸들러 추가

---
+++ src/mocks/handlers/adminUsers/handler.js
@@ -0,0 +1,11 @@
+import { rest } from 'msw';
+import { userListResponse } from './response';
+
+export default [
+  rest.get('/admin/v1/users', (req, res, ctx) => {
+    // 유저리스트 API
+    // /admin/v1/users GET
+
+    return res(ctx.status(200), ctx.json(userListResponse));
+  }),
+];
