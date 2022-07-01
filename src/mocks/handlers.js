acec8f32514741d499378d31c79713ffc876546c
Jayden <cjy0029@naver.com>
2022-07-01 09:26:39 +0900

    feat : review 작성하기 api 연결완료

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,4 +1,9 @@
 import lecturesHandler from './handlers/lecturesHandler/handler';
 import lectureDetailHandler from './handlers/lectureDetailHandler/handler';
+import createReviewHandler from './handlers/createReview/handler';
 
-export const handlers = [...lecturesHandler, ...lectureDetailHandler];
+export const handlers = [
+  ...lecturesHandler,
+  ...lectureDetailHandler,
+  ...createReviewHandler,
+];
